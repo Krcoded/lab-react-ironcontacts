98291071aa7404539347ca0b5cb238e6f713128d
Kevin Rodriguez <87106659+Krcoded@users.noreply.github.com>
2021-09-08 11:41:23 -0400

    completed lab with react working delete, sort, random

--- src/components/ActorsData.js
+++ src/components/ActorsData.js
@@ -1,17 +1,57 @@
 import { useState } from "react";
-
 import actorsList from "../contacts.json";
 
+const initContacts = actorsList.splice(0, 5);
+
 function ActorsData() {
-  const [actors, setActors] = useState(actorsList);
+  const [actors, setList] = useState(initContacts);
+  const [outRemaining, setOutRemaining] = useState(actorsList);
+
+  function addRandom() {
+    if (outRemaining && outRemaining.length > 0) {
+      let index = Math.floor(Math.random() * (outRemaining.length - 0) + 0);
+      setList([...actors, outRemaining[index]]);
+      let filtered = outRemaining.filter((contact, i) => {
+        return i !== index;
+      });
+      setOutRemaining(filtered);
+    }
+  }
+
+  function byPopularity() {
+    let sorted = actors.sort((a, b) => b.popularity - a.popularity);
+    setList([...sorted]);
+  }
+
+  function byName() {
+    let sorted = actors.sort((a, b) => a.name.localeCompare(b.name));
+    setList([...sorted]);
+  }
+
+  function deleteActor(id) {
+    let filtered = actors.filter((actors) => {
+      return actors.id !== id;
+    });
+    setList(filtered);
+  }
 
   return (
-    <div>
+    <div className="table">
       <h2>IronContacts</h2>
+
       <div>
-        <button className="btn-top"> Add Random Contact </button>
-        <button className="btn-top"> Sort by Popularity </button>
-        <button className="btn-top"> Sort by Name </button>
+        <button onClick={addRandom} className="btn-top">
+          {" "}
+          Add Random Contact{" "}
+        </button>
+        <button onClick={byPopularity} className="btn-top">
+          {" "}
+          Sort by Popularity{" "}
+        </button>
+        <button onClick={byName} className="btn-top">
+          {" "}
+          Sort by Name{" "}
+        </button>
       </div>
 
       {actors.map((actors) => {
@@ -19,10 +59,10 @@ function ActorsData() {
           <table>
             <tr>
               <th> Picture </th>
-              <th> Name: </th>
-              <th>Popularity:</th>
-              <th> wonOscar</th>
-              <th> wonEmmy</th>
+              <th> Name </th>
+              <th>Popularity</th>
+              <th> Won Oscar</th>
+              <th> Won Emmy</th>
               <th> Actions </th>
             </tr>
             <tr>
@@ -37,11 +77,17 @@ function ActorsData() {
               </td>
               <td> {actors.name} </td>
               <td> {actors.popularity}</td>
-              <td>{actors.wonOscar} 🏆</td>
-              <td>{actors.wonEmmy}🏆</td>
+              <td>{actors.wonOscar ? "🏆" : "X"}</td>
+              <td>{actors.wonEmmy ? "🏆" : "X"}</td>
               <td>
                 {" "}
-                <button class="btn-delete"> Delete </button>{" "}
+                <button
+                  onClick={() => deleteActor(actors.id)}
+                  class="btn-delete"
+                >
+                  {" "}
+                  Delete{" "}
+                </button>{" "}
               </td>
             </tr>
           </table>
